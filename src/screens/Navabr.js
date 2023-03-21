600d8b9a9535e43033b8de84681f837e63612f49
umaransar03 <umaransarmusic@gmail.com>
2023-03-21 14:19:03 -0400

    changes made

--- src/screens/Navabr.js
+++ src/screens/Navabr.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import logo from '../images/logo_header.png';
 import './Navbar.css';
+import MenuIcon from '@mui/icons-material/Menu';
 
 export const Navabr = () => {
     return (
         <header className='header'>
-            <div className=' '>
+            <div className='logo-header'>
                 <a href='#'>
                     <img src={logo} alt='' width={200} height={50}></img>
                 </a>
             </div>
+            
             <nav class="nav">
-                <input type="checkbox" className='toggle-menu'  />
+                {/* <div className='hamburger' ><MenuIcon /></div> */}
+                <input type="checkbox" className='toggle-menu'/>
                 <div className='hamburger'></div>
                 <ul className='menu'>
                     <li><a className='menuItems' href="#">Home</a></li>
@@ -19,6 +22,9 @@ export const Navabr = () => {
                     <li><a className='menuItems' href="#">Book</a></li>
                     <li><a className='menuItems' href="#">About</a></li>
                     <li><a className='logIn' href="#">Log In</a></li>
+                </ul>
+                <ul className='log'>
+                    
                 </ul>
             </nav>
         </header>
