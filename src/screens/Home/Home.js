4c5628bfddde4f026f46047da3bed081774ed3a5
umaransar03 <umaransarmusic@gmail.com>
2023-03-22 12:40:52 -0400

    just did it yeahhh

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -4,32 +4,41 @@ import { Navabr } from '../Navabr';
 import mainPhoto from '../images/1.jpg';
 
 export const Home = () => {
-  return (
-    <div>
-        <Navabr/>
-        <main className='main'>
-            <div className='topSection'>
-                <div className='top'>
-                <div className='shortIntro'>
-                    <h2>
-                        Little Lemon
-                    </h2>
-                    <h4>Chicago</h4>
-                    <h5>
-                        Little Lemon is a fresh and healthy restaurant serving 
-                        plant-based and gluten-free options. Their locally sourced
-                        ingredients make for a delicious and nutritious meal. Enjoy 
-                        a cozy atmosphere for breakfast, lunch, or dinner.
-                    </h5>
-                    <button className='table'>Reserve a Table</button>
-                </div>
-                <img src={mainPhoto} alt="" width={300} height={400}></img>
+    return (
+        <div>
+            <Navabr />
+            <div>
+                <main className='main'>
+                    <div className='topSection'>
+                        <div className='top'>
+                            <div className='shortIntro'>
+                                <h2>
+                                    Little Lemon
+                                </h2>
+                                <h4>Chicago</h4>
+                                <h5>
+                                    Little Lemon is a fresh and healthy restaurant serving
+                                    plant-based and gluten-free options. Their locally sourced
+                                    ingredients make for a delicious and nutritious meal. Enjoy
+                                    a cozy atmosphere for breakfast, lunch, or dinner.
+                                </h5>
+                                <button className='table'>Reserve a Table</button>
+                            </div>
+                            <img src={mainPhoto} alt="" width={300} height={400}></img>
+                        </div>
+                    </div>
+                    <section className='topSection'>
+
+                    </section>
+                </main>
+            </div>
+            <div>
+                <div className='menuu'>
+                    <h1>Specials</h1>
+                    <button>Menu</button>
                 </div>
             </div>
-            <section className='topSection'>
-                
-            </section>
-        </main>
-    </div>
-  )
+            
+        </div>
+    )
 }
